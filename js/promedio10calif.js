5ce71388ce868f32135f2e4e24e3654ee97970fa
MunguiaOviedoElvaMelina <79117100+MunguiaOviedoElvaMelina@users.noreply.github.com>
2021-03-25 18:44:28 -0600

    Update promedio10calif.js

---
+++ js/promedio10calif.js
@@ -0,0 +1,29 @@
+<script>
+
+  
+  //programa 2.- Leer 10 calificaciones, mostrar el promedio y las calificaciones mayores al promedio
+  //Munguia Oviedo Elva Melina
+  
+  
+  let calificaciones=[];
+  let califmayores=[];
+  
+	for(let i=0;i<10;i++){
+    let leer=parseFloat(prompt("Teclea la calificación"+(i+1)));
+    calificaciones.push(leer);
+  }
+  
+  let suma=0;
+  let promedio;
+  for(let i=0;i<10;i++){
+   suma+=calificaciones[i];
+   }
+   promedio=suma/10;
+  for(let i=0;i<10;i++){
+   if(promedio<=calificaciones[i]){
+       califmayores.push(calificaciones[i]);}
+  }
+  document.write("<br>Su promedio es "+promedio+"<br> Calificaciones mayores: "+califmayores);
+  
+  
+</script>
